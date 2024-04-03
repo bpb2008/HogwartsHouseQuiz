88054ed81af14462fbf8db0bb403185113136c77
bpb2008 <brittanythompson08@gmail.com>
2024-04-03 11:14:43 -0500

    updated README

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Which Hogwarts House Do You Belong To?
 
-Testing out a simple quiz app using React and Vite. I'm borrowing some of the questions and answers from this [BuzzFeed Quiz](https://www.buzzfeed.com/eleanorbate/accurate-af-sorting-quiz). Trying to practice the logic here, not really be creative with the questions and answers! 
+Testing out a simple quiz app using React and Vite. I'm borrowing some of the questions and answers from this [BuzzFeed Quiz](https://www.buzzfeed.com/eleanorbate/accurate-af-sorting-quiz). Trying to practice the logic here, not really be creative with the questions and answers!
+
+How to run this project locally:
+
+1. Clone the project.
+
+2. Install dependencies by running `npm install`.
+
+3. Run `npm run dev` and launch this front-end only web app in your browser.
--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,35 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 import Question from "./components/Question";
+import { quiz } from "./quiz";
 import "./App.css";
 
 const App = () => {
   const [start, setStart] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [results, setResults] = useState();
+  const [results, setResults] = useState([]);
+  const [showResults, setShowResults] = useState(false);
 
   const clickToStart = () => {
     setStart(true);
   };
+
+  const handleRestart = () => {
+    setCurrentQuestion(0);
+    setResults([]);
+    setShowResults(false);
+  };
+
+  const calculateResult = () => {
+    const total = results.reduce((acc, value) => acc + value, 0);
+    return total;
+  };
+
+  const showResult = () => {
+    setShowResults(true);
+  };
+
+  console.log(currentQuestion);
 
   return (
     <Container
@@ -37,16 +56,30 @@ const App = () => {
         >
           What is your Hogwarts House?
         </Typography>
-        {start ? (
+        {start && !showResults ? (
           <Question
             currentQuestion={currentQuestion}
             setCurrentQuestion={setCurrentQuestion}
             results={results}
             setResults={setResults}
           />
+        ) : showResults ? (
+          <Box>
+            <Typography variant="h4">
+              Your Hogwarts House is: {calculateResult()}
+            </Typography>
+            <Button variant="contained" onClick={handleRestart()}>
+              Restart Quiz
+            </Button>
+          </Box>
         ) : (
           <Button variant="contained" onClick={clickToStart}>
             Start Quiz!
+          </Button>
+        )}
+        {start && currentQuestion === quiz.questions.length - 1 && (
+          <Button variant="contained" onClick={showResult}>
+            Show Results
           </Button>
         )}
       </Box>
